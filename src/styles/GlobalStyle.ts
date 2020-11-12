40d14d117973b1ab9b19c4b62c4e7418d8ca5532
TecJP <tec.joao.freitas@gmail.com>
2020-11-12 09:07:50 -0300

    Reset css in globalstyles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -5,11 +5,18 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    outline: 0;
+    box-sizing: border-box;
   }
 
   body {
     background: #4F4F4F;
     color: #fff;
     font-family: Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body html #root {
+    height: 100%;
   }
 `;
